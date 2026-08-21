6a8573017450916108ef342103ef01246b80e3c7
srjonjoy <ghoshsrinjoy2@gmail.com>
2026-08-21 20:12:50 +0530

    added

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cookieparser())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-resumebuilder-six.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
